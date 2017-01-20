15c60313eb06f4c63051ccbcde5ae1656f3daa4f
ali <ali@jafari.pw>
2017-01-20 13:42:12 +0330

    files created

---
+++ GeneticAlgorithm.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * The main class is this
+ * this class is going to calculate the algorithm
+ * 
+ * Created by PhpStorm.
+ * User: ali
+ * Date: 1/20/2017
+ * Time: 13:41:20
+ */
+class GeneticAlgorithm
+{
+
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This index is going to use GA class and show the
+ * response and draw the chart
+ *
+ * Created by PhpStorm.
+ * User: ali
+ * Date: 1/20/2017
+ * Time: 13:39:58
+ */
+
