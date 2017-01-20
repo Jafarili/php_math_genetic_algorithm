0146050be3a91cd35cf3bd7e13fe067e479f3d68
ali <ali@jafari.pw>
2017-01-20 14:19:49 +0330

    cumulative probability

--- GeneticAlgorithm.php
+++ GeneticAlgorithm.php
@@ -18,6 +18,9 @@ class GeneticAlgorithm
     public $chromosome = array();
     public $fx = array();
     public $fitness = array();
+    public $probability = array();
+    public $cumulative_probability = array();
+
     public $total_fitness = 0;
 
     public function __construct()
@@ -28,8 +31,9 @@ class GeneticAlgorithm
         $this->calcFx();
 
         $this->calcFitness();
+        $this->calcProbability();
 
-        var_dump($this->fitness);
+        var_dump($this->cumulative_probability);
     }
 
     public function calcFx(){
@@ -41,6 +45,15 @@ class GeneticAlgorithm
         for ($i=0;$i<5;$i++) {
             $this->fitness[$i] = 1 / (1 + $this->fx[$i]);
             $this->total_fitness += $this->fitness[$i];
+        }
+    }
+
+    public function calcProbability(){
+        $sum = 0;
+        for ($i=0;$i<5;$i++) {
+            $this->probability[$i] = $this->fitness[$i] / $this->total_fitness;
+            $sum += $this->probability[$i];
+            $this->cumulative_probability[$i] = $sum;
         }
     }
 }
