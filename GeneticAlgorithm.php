c02715ac58a78862a978b531e661e6d8d077ec4d
ali <ali@jafari.pw>
2017-01-20 14:04:46 +0330

    fx and fitness

--- GeneticAlgorithm.php
+++ GeneticAlgorithm.php
@@ -11,13 +11,33 @@
  */
 class GeneticAlgorithm
 {
+    public $a;
+    public $b;
+    public $c;
+
     public $chromosome = array();
+    public $fx = array();
+    public $fitness = array();
 
     public function __construct()
     {
         for ($i=0;$i<5;$i++)
-            $chromosome[$i] = array(rand(0,10),rand(0,10),rand(0,10));
+            $this->chromosome[$i] = array(rand(0,10),rand(0,10),rand(0,10));
 
-        var_dump($chromosome);
+        $this->calcFx();
+
+        $this->calcFitness();
+
+        var_dump($this->fitness);
+    }
+
+    public function calcFx(){
+        for ($i=0;$i<5;$i++)
+            $this->fx[$i] = abs(($this->chromosome[$i][0] + ($this->chromosome[$i][1] * 2) + ($this->chromosome[$i][2] * 3)) - 10);
+    }
+
+    public function calcFitness(){
+        for ($i=0;$i<5;$i++)
+            $this->fitness[$i] = 1/(1+$this->fx[$i]);
     }
 }
